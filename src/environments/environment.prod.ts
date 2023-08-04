3959dc59ce2858f1189cb0a6bb9846afe885f325
StonyDigi <stony86@gmail.com>
2023-08-04 12:35:50 +0200

    done commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-chat-app-231e8',
+    appId: '1:542941297142:web:ffddf8530e88a0ba146fd7',
+    storageBucket: 'angular-chat-app-231e8.appspot.com',
+    apiKey: 'AIzaSyCWWlDQ3xWIGXD0BIdYWpW9wUPPuE1-lxg',
+    authDomain: 'angular-chat-app-231e8.firebaseapp.com',
+    messagingSenderId: '542941297142',
+  },
+  production: true,
+  apiUrl: 'https://us-central1-get-stream-io-decoded.cloudfunctions.net',
+  stream: {
+    key: 'wxgsgqrws5v8'
+  }
+};
